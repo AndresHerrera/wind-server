79702b8fdc50656a725a0352b0a7c9a2346e8b8f
AndresHerrera <t763rm3n@gmail.com>
2021-05-05 18:08:03 -0500

    add localhost:80 to white list

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ const whitelist = [
     "http://localhost:8080",
     "http://localhost:3000",
     "http://localhost:4000",
+    "http://localhost:80",
+    "http://localhost",
 ];
 
 const corsOptions = {
